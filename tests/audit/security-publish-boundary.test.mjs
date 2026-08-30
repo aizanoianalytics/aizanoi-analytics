ce57bea076ef020ab924814f7ea790427783c650
aizanoianalytics <huseyiin.erduran@gmail.com>
2026-08-30 21:37:02 +0300

    test: allow Camera public manifest path

--- tests/audit/security-publish-boundary.test.mjs
+++ tests/audit/security-publish-boundary.test.mjs
@@ -54,6 +54,7 @@ const ALLOWED_JSON = [
   /\/news\/index\.json$/,
   /\/news\/.+\/index\.json$/,
   /\/analytics\/.+\/data\.json$/,
+  /\/js\/v3\/apps\/camera\/manifest\.json$/,
   /\/js\/v3\/apps\/notepad\/manifest\.json$/,
   /\/js\/v3\/apps\/recycle-bin\/manifest\.json$/,
   /\/js\/v3\/apps\/winamp\/manifest\.json$/
